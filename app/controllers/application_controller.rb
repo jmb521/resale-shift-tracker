e7591237266d3547d14362541422d046b6dade32
github username <github email address>
2019-09-15 20:28:29 -0500

    added sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "resale is fun"
   end
 
   get "/" do
