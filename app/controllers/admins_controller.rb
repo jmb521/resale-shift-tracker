95ea30a13dcb1bf3edede8ed10174e179c548515
github username <github email address>
2019-09-15 21:51:25 -0500

    added code for forms (edit and new)

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,32 +1,50 @@
 class AdminsController < ApplicationController
 
   # GET: /admins
-  get "/admins" do
-    erb :"/admins/index.html"
-  end
+  # get "/admins" do
+  #   erb :"/admins/index.html"
+  # end
 
   # GET: /admins/new
-  get "/admins/new" do
+  get "/signup" do
     erb :"/admins/new.html"
   end
 
   # POST: /admins
   post "/admins" do
-    redirect "/admins"
+
+    @admin = Admin.new(params[:admin])
+    if @admin.save
+      redirect '/resales'
+    else
+      redirect '/signup'
+    end
   end
 
   # GET: /admins/5
   get "/admins/:id" do
+    #admin profile page
+    @admin = Admin.find(params[:id])
     erb :"/admins/show.html"
   end
-
+  
   # GET: /admins/5/edit
   get "/admins/:id/edit" do
+    @admin = Admin.find(params[:id])
     erb :"/admins/edit.html"
   end
-
+  
   # PATCH: /admins/5
   patch "/admins/:id" do
+    @admin = Admin.find(params[:id])
+    if !params[:admin][:password].empty? && !params[:admin][:new_password].empty?
+      if @admin.authenticate(params[:admin][:password])
+        @admin.update(:first_name => params[:admin][:first_name], :last_name => params[:admin][:last_name], :username => params[:admin][:username], :password => params[:admin][:new_password])
+        #!add flash messages verifying profile is updated
+      else
+        redirect "/admins/#{@admin.id}"
+        #!add flash message that the password they entered was wrong. 
+
     redirect "/admins/:id"
   end
 
